f578b170693416af8d539f76733f3036d7ba4055
Ballauma <1271044523@qq.com>
2026-02-10 16:11:32 +0800

    缓存套餐功能开发

--- yorozuya-server/src/main/java/com/yorozuya/YorozuyaApplication.java
+++ yorozuya-server/src/main/java/com/yorozuya/YorozuyaApplication.java
@@ -3,6 +3,7 @@ package com.yorozuya;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement // 开启注解方式的事务管理
 @Slf4j
+@EnableCaching
 public class YorozuyaApplication {
     public static void main(String[] args) {
         SpringApplication.run(YorozuyaApplication.class, args);
